1a61f631499a6e1778c766f11170626959fad87f
Sam King <kingst@gmail.com>
2013-10-21 21:26:24 -0700

    stop overwriting global variable 'status'

--- hw.js
+++ hw.js
@@ -1,15 +1,15 @@
 (function() {
-    var status = document.getElementById("status_div");
+    var stat = document.getElementById("status_div");
     if (!this.adrenaline) {
-        status.innerHTML = "Adrenaline library not found<br>" +
+        stat.innerHTML = "Adrenaline library not found<br>" +
             "<b>Make sure you copy adrenaline.min.js to the top level directory</b>";
         return;
     }
-    status.innerHTML = "Checking server...";
+    stat.innerHTML = "Checking server...";
     var promise = this.adrenaline.post("apis/users/login_challenge");
     promise.done(function() {
-        status.innerHTML = "All systems are go!";
+        stat.innerHTML = "All systems are go!";
     }).fail(function(err) {
-        status.innerHTML = "error -> <b>" + err + "</b>";
+        stat.innerHTML = "error -> <b>" + err + "</b>";
     });
 })();
