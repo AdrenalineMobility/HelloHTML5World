3733ca7917e10fff86517f981b5b2ea0bbf888c6
murph <murph@murph.cc>
2013-10-24 11:43:35 -0700

    Use getAppDetails as example API call.
    Use less verbose syntax, too.
    
    Hello, git world! ;)

--- hw.js
+++ hw.js
@@ -1,15 +1,21 @@
 (function() {
     var stat = document.getElementById("status_div");
+
+    /* Check if adrenaline is loaded */
     if (!this.adrenaline) {
         stat.innerHTML = "Adrenaline library not found<br>" +
             "<b>Make sure you copy adrenaline.min.js to the top level directory</b>";
         return;
     }
+
+    /* Perform a simple API call */
     stat.innerHTML = "Checking server...";
-    var promise = this.adrenaline.post("apis/users/login_challenge");
-    promise.done(function() {
-        stat.innerHTML = "All systems are go!";
-    }).fail(function(err) {
-        stat.innerHTML = "error -> <b>" + err + "</b>";
-    });
+    adrenaline.getAppDetails()
+        .done(function(details) {
+            stat.innerHTML = "<p>API Call Completed Successfully!" +
+                "<p> App Name: " + details['name'];
+        })
+        .fail(function(err) {
+            stat.innerHTML = "error -> <b>" + err + "</b>";
+        });
 })();
